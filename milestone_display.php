d305039bd844d4fc0095f78fedae02c54352bc62
Tomasz Kalkosinski <pppspoonman@users.sourceforge.net>
2008-11-25 23:34:15 +0000

    Forgot to commit this one.

---
+++ milestone_display.php
@@ -0,0 +1,29 @@
+<?php
+// $Header: /cvsroot/bitweaver/_bit_tickets/milestone_display.php,v 1.1 2008/11/25 23:34:15 pppspoonman Exp $
+// Copyright (c) 2004 bitweaver Tickets
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+
+// Initialization
+require_once( '../bit_setup_inc.php' );
+
+// Is package installed and enabled
+$gBitSystem->verifyPackage( 'tickets' );
+
+// Look up the content
+require_once( TICKETS_PKG_PATH.'lookup_milestone_inc.php' );
+
+if( !$gContent->isValid() ) {
+	$gBitSystem->setHttpStatus( 404 );
+	$gBitSystem->fatalError( "The tickets you requested could not be found." );
+}
+
+// Now check permissions to access this content 
+$gContent->verifyViewPermission();
+
+// Add a hit to the counter
+$gContent->addHit();
+
+// Display the template
+$gBitSystem->display( 'bitpackage:tickets/milestone_display.tpl', tra( 'Tickets' ) , array( 'display_mode' => 'display' ));
+?>
