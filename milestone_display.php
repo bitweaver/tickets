1d500bff7f7cf9a9b47598045a4bcda17e38487b
Tomasz Kalkosinski <pppspoonman@users.sourceforge.net>
2008-11-25 23:38:35 +0000

    Fetch field definitions to display ticket list properly.

--- milestone_display.php
+++ milestone_display.php
@@ -1,5 +1,5 @@
 <?php
-// $Header: /cvsroot/bitweaver/_bit_tickets/milestone_display.php,v 1.1 2008/11/25 23:34:15 pppspoonman Exp $
+// $Header: /cvsroot/bitweaver/_bit_tickets/milestone_display.php,v 1.2 2008/11/25 23:38:35 pppspoonman Exp $
 // Copyright (c) 2004 bitweaver Tickets
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
@@ -15,7 +15,7 @@ require_once( TICKETS_PKG_PATH.'lookup_milestone_inc.php' );
 
 if( !$gContent->isValid() ) {
 	$gBitSystem->setHttpStatus( 404 );
-	$gBitSystem->fatalError( "The tickets you requested could not be found." );
+	$gBitSystem->fatalError( "The milestones you requested could not be found." );
 }
 
 // Now check permissions to access this content 
@@ -23,6 +23,11 @@ $gContent->verifyViewPermission();
 
 // Add a hit to the counter
 $gContent->addHit();
+
+// Get field definitions for listing tickets.
+$ticket = new BitTicket ();
+$fieldDefinitions = $ticket->getFieldDefinitions();
+$gBitSmarty->assign_by_ref( 'fieldDefinitions' , $fieldDefinitions );
 
 // Display the template
 $gBitSystem->display( 'bitpackage:tickets/milestone_display.tpl', tra( 'Tickets' ) , array( 'display_mode' => 'display' ));
